f70c09164cc3841ee343f1ad8790ee5e13d2e33f
darkreplive <darkreplive@gmail.com>
2022-01-22 16:31:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("1 - Import Words from file");
+Console.WriteLine("2 - Bubble Sort Words");
+Console.WriteLine("3 - LINQ/Lamdba sort Words");
+Console.WriteLine("4 - Count the Distinct Words");
+Console.WriteLine("5 - Get and display words that start with 'j' and display the count");
+Console.WriteLine("7 - Get and display words that end with 'd' and display the count");
+Console.WriteLine("8 - Get and display words that are greater than 4 characters long, and display the count");
+Console.WriteLine("5 - Get and display words that start with 'j' and display the count");
+Console.WriteLine("9 - Get and display words that are less than 3 characters long and start with the letter 'a', and display the count");
+Console.WriteLine("x – Exit");
+string user = Console.ReadLine();
+Console.WriteLine("Select Option" + user);
+
+while(user!= "x")
+{
+
+}
